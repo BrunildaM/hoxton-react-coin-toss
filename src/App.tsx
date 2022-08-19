281cb517953a59b7ec7c70e18c22960d3a48d9fd
Brunilda Muco <brunildamuco6@gmail.com>
2022-08-19 13:06:12 +0200

    added some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
-
-import { useState } from 'react';
-import './App.css'
-
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [side, setSide] = useState(1)
-  const [tossed, setTossed] = useState(0)
+  const [side, setSide] = useState(1);
+  const [tossed, setTossed] = useState(0);
 
   const tossCoin = () => {
-    const landedOn = Math.round(Math.random())
-   setSide(landedOn)
-    setTossed(tossed + 1)
-  }
+    const landedOn = Math.round(Math.random());
+    setSide(landedOn);
+    setTossed(tossed + 1);
+  };
 
- let eagle = <img src='https://i.colnect.net/f/5450/348/10-Qindarka-10-Qindar-Leku.jpg' alt='' width={150} />
- let lion = <img src='https://i.pinimg.com/236x/53/c0/96/53c09607cf6fa7c439a22cbd13d154f1--rare-coins-albania.jpg' alt='' width={150}/>
-
+  let eagle = (
+    <img
+      src="https://i.colnect.net/f/5450/348/10-Qindarka-10-Qindar-Leku.jpg"
+      alt=""
+      width={150}
+    />
+  );
+  let lion = (
+    <img
+      src="https://i.pinimg.com/236x/53/c0/96/53c09607cf6fa7c439a22cbd13d154f1--rare-coins-albania.jpg"
+      alt=""
+      width={150}
+    />
+  );
 
   return (
-    <div>
-      <button onClick={tossCoin}>Toss coin</button>
+    <div className="App">
       <p>{side === 1 ? eagle : lion}</p>
+      <button
+      className="button"
+      onClick={tossCoin}>Toss coin</button>
       <p>The coin has been tossed {tossed} times.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
